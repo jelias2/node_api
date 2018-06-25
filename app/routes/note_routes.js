28f104108955b8fcb84d4e4ef3e2bba2729fea37
Elias <n0342942@VDDP14X-GGO3RU5.lm.lmig.com>
2018-06-25 12:20:56 -0400

    JSON and text I/0 working

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -6,9 +6,27 @@ module.exports = function( app, db) {
 
     //Request Json body from Api
     console.log(req.body)
+    console.log("Does it come from here?")
 
+    prompt('Whats your name?', function (input) {
+    console.log(input);
+    //process.exit();
+    });
     //Send Hello to endpoint
-    res.send('Hello')
+    //res.send('Hello')
   })
 
 };
+
+//This function is used to perform text I/O
+function prompt(question, callback) {
+    var stdin = process.stdin,
+        stdout = process.stdout;
+
+    stdin.resume();
+    stdout.write(question);
+
+    stdin.once('data', function (data) {
+        callback(data.toString().trim());
+    });
+}
